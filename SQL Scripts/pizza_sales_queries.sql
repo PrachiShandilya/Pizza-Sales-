15acde48c3fc51bad31d8ad3bb7e10e074f1b9b6
PrachiShandilya <53363152+PrachiShandilya@users.noreply.github.com>
2026-04-16 18:50:22 +0530

    Update pizza_sales_queries.sql

--- SQL Scripts/pizza_sales_queries.sql
+++ SQL Scripts/pizza_sales_queries.sql
@@ -5,7 +5,7 @@ USE pizzadb;
 SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
  
 -- 2. Avg Order Value:
-SELECT SUM(total_price) / COUNT(DISTINCT order_id) FROM pizza_sales;
+SELECT SUM(total_price) / COUNT(DISTINCT order_id) AS Avg_Order_Value FROM pizza_sales;
  
 -- 3. Total Pizzas Sold:
 SELECT SUM(quantity) AS Total_Pizzas_Sold FROM pizza_sales;
